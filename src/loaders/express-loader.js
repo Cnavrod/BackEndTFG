d5b7607e371c1072800571c3b347ec050a3efcf4
Carlos Navarro R. <152632552+Cnavrod@users.noreply.github.com>
2025-05-29 20:55:12 +0200

    corss 3

--- src/loaders/express-loader.js
+++ src/loaders/express-loader.js
@@ -10,24 +10,21 @@ import playlistRouter from '../routes/playlist-routes.js';
 import newsletterTemplateRouter from '../routes/newsletter-routes.js';
 
 export default function (server) {
-server.use(cors({
-  origin: ['http://localhost:3000', 'https://front-end-tfg.vercel.app/'], // añade aquí tus orígenes permitidos
-  credentials: true
-}));
+  server.use(cors({
+    origin: ['http://localhost:3000', 'https://front-end-tfg.vercel.app'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
   server.use(express.json());
   server.use(express.urlencoded({ extended: true }));
-  /* Static files */
   server.use(express.static('public'));
-  /* Swagger */
   server.get('/docs', (req, res) => res.send(swaggerDoc));
   server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-  /* MDW */
   server.use(morganMiddleware);
-  /* Routes */
   server.use('/api/newsletter-templates', newsletterTemplateRouter);
-  server.use('/api/users', userRouter); // Asegúrate de que las rutas están bajo el prefijo /api
-  server.use('/api/songs', songRouter); // Asegúrate de que las rutas están bajo el prefijo /api
+  server.use('/api/users', userRouter);
+  server.use('/api/songs', songRouter);
   server.use('/api/playlists', playlistRouter);
-  /* Error handler */
   server.use(errorMiddleware);
 }
