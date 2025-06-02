a001cc21f2f137a3fb9931288ac908ded9ff603d
Carlos Navarro R. <152632552+Cnavrod@users.noreply.github.com>
2025-06-02 21:32:38 +0200

    cors :(

--- src/loaders/express-loader.js
+++ src/loaders/express-loader.js
@@ -11,7 +11,7 @@ import newsletterTemplateRouter from '../routes/newsletter-routes.js';
 
 export default function (server) {
   server.use(cors({
-    origin: ['http://localhost:3000', 'https://front-end-tfg.vercel.app'],
+    origin: ['http://localhost:3000', 'https://front-end-tfg.vercel.app', 'https://tfgcarlosnavarro.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
